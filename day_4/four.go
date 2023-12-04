2eae1e5428f979937a129cd4f7c9e96356c6d5e0
Benjamin King <benjamin.allan.king@gmail.com>
2023-12-04 12:35:15 +0000

    Day 4

--- day_4/four.go
+++ day_4/four.go
@@ -9,11 +9,11 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/baking-code/godash"
+	"github.com/baking-code/godash/some"
 )
 
 func main() {
-	file, err := os.Open("./test.txt")
+	file, err := os.Open("./data.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -23,7 +23,9 @@ func main() {
 	// digits := regexp.MustCompile("(\\d+)")
 
 	totalScore := 0
+	lineNo := 0
 	for scanner.Scan() {
+		lineNo++
 		line := scanner.Text()
 		colonIndex := strings.Index(line, ":")
 		allNumbers := line[colonIndex+1:]
@@ -33,13 +35,15 @@ func main() {
 		numberOfWinners := 0
 		for _, play := range playNumbers {
 
-			if godash.Some[int64](winningNumbers, func(e int64, index int, collection []int64) bool {
+			if some.Some[int64](winningNumbers, func(e int64, index int, collection []int64) bool {
 				return e == play
 			}) {
 				numberOfWinners++
 			}
 		}
-		totalScore += int(math.Pow(2.0, float64(numberOfWinners)))
+		gameScore := int(math.Pow(2.0, float64(numberOfWinners)-1))
+		totalScore += gameScore
+		fmt.Println("line", lineNo, "winners", numberOfWinners, "score", gameScore)
 	}
 
 	fmt.Println("Total score", totalScore)
@@ -53,7 +57,7 @@ func splitNumbers(s string) []int64 {
 	strings := strings.Split(s, " ")
 	numbers := make([]int64, 0)
 	for _, str := range strings {
-		if str != " " {
+		if str != " " && str != "" {
 			num, err := strconv.ParseInt(str, 10, 0)
 			if err != nil {
 				fmt.Println(err)
--- go.mod
+++ go.mod
@@ -2,7 +2,6 @@ module advent-of-code-2023
 
 go 1.21.4
 
-require (
-	github.com/baking-code/godash v0.1.0 // indirect
-	golang.org/x/exp v0.0.0-20231127185646-65229373498e // indirect
-)
+require golang.org/x/exp v0.0.0-20231127185646-65229373498e
+
+require github.com/baking-code/godash v0.1.1 // indirect
